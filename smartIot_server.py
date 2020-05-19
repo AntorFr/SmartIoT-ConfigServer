0ec7215d1ccf84bbf2530a44c57e5f422a3ecc57
AntorFR <contact@antor.fr>
2020-05-19 19:34:48 +0200

    First functional release

---
+++ smartIot_server.py
@@ -0,0 +1,67 @@
+
+import paho.mqtt.client as mqtt
+import json
+
+from smartiot_firmware import FirmwareWatcher 
+
+config = {}
+
+def load_config():
+    global config
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+        if ("port" not in config["broker"]):
+            config["broker"]["port"] = 1883
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    client.subscribe(config["domains"][0]+"/setup/ota/#")
+    print("suscribed to: "+config["domains"][0]+"/setup/ota/#")
+    post_firmwares()
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    #print(msg.topic+" "+str(msg.payload))
+    return
+
+def on_firmware_message(client, userdata, msg):
+   topic=  msg.topic.split("/") 
+   if (msg.retain == True and ( \
+        topic[3] not in fw.firmwares or \
+        topic[5] != fw.firmwares[topic[3]]["md5"])):
+        print("firmware on "+msg.topic+" outdated, remove it")
+        client.publish(msg.topic, payload="", qos=1, retain=True)
+
+
+def post_firmwares():
+    for fwname, firmware in fw.firmwares.items():
+        for domain in config["domains"]:
+            client.publish(domain+"/setup/ota/"+fwname+"/firmware", payload=json.dumps(firmware), qos=1, retain=True)
+            client.publish(domain+"/setup/ota/"+fwname+"/firmware/"+firmware["md5"], payload=fw.get_firmware_data(firmware["path"]), qos=1, retain=True)
+        firmware["published"]=True
+    
+
+
+if __name__ == "__main__":
+    load_config()
+
+    fw = FirmwareWatcher("./firmwares")
+    print(fw.firmwares)
+
+    client = mqtt.Client(client_id="SmartIot_configServ")
+    client.will_set(config["domains"][0]+"/setup/up/smartiot_configserv", payload='{"status":"Offline"}', qos=1, retain=True)
+    if ("username" in config["broker"]):
+        client.username_pw_set(config["broker"]["username"], password=config["broker"]["password"])
+    client.on_connect = on_connect
+    client.on_message = on_message
+    for domain in config["domains"]:
+        client.message_callback_add(domain+"/setup/ota/+/firmware/+", on_firmware_message)
+
+    client.connect(config["broker"]["host"], config["broker"]["port"], 60)
+
+    try:
+        client.loop_forever()
+    except (SystemExit, KeyboardInterrupt):
+        fw.observer.stop()
+        client.disconnect()
+    fw.observer.join()
---
+++ smartiot_firmware.py
@@ -0,0 +1,123 @@
+
+import re, sys, os
+import base64, sys, math
+from hashlib import md5
+
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+
+class FirmwareWatcher:
+    def __init__(self, directory):
+        self._directory = directory
+        self.firmwares = {}
+
+        self._read_firmware_folder()
+
+        self.event_handler = PatternMatchingEventHandler(
+            patterns=["*.bin"],ignore_directories=True)
+        self.event_handler.on_created = self.on_created
+        self.event_handler.on_deleted = self.on_deleted
+        self.event_handler.on_modified = self.on_modified
+        self.event_handler.on_moved = self.on_moved
+
+        self.observer = Observer()
+        self.observer.schedule(self.event_handler, self._directory,
+                               recursive=False)
+
+        self.observer.start() 
+
+    def on_created(self,event):
+        print("File {0} created, update firmware info.".format(event.src_path))
+        self._update_firmware_info(event.src_path) 
+
+    def on_modified(self,event):
+        print("File {0} modified, update firmware info.".format(event.src_path))
+        self._update_firmware_info(event.src_path) 
+
+    def on_deleted(self,event):
+        print("File {0} deleted, update firmware info.".format(event.src_path))
+        #todo delete file from firmware list
+        #self._read_firmware_folder() 
+
+    def on_moved(self,event):
+        print("File {0} moved, update firmware info.".format(event.src_path))
+        self._read_firmware_folder()  
+
+    @staticmethod
+    def version_toint(version):
+        v = version.split(".")
+        return sum([int(val)*(100**int(3-idx)) for idx, val in enumerate(v)])
+
+    @staticmethod
+    def get_firmware_data(path):
+        try:
+            firmware_file = open(path, "rb")
+        except Exception as err:
+            print("Error: {0}".format(err))
+
+        firmware_binary = firmware_file.read()
+        firmware_file.close()
+        firmware = bytearray()
+        firmware.extend(firmware_binary)
+        return firmware
+
+    def _update_firmware_info(self,path):
+        firmware = {}
+
+        regex_SmartIoT = re.compile(b"\x25\x48\x4f\x4d\x49\x45\x5f\x45\x53\x50\x38\x32\x36\x36\x5f\x46\x57\x25")
+        regex_name = re.compile(b"\xbf\x84\xe4\x13\x54(.+)\x93\x44\x6b\xa7\x75")
+        regex_version = re.compile(b"\x6a\x3f\x3e\x0e\xe1(.+)\xb0\x30\x48\xd4\x1a")
+        regex_brand = re.compile(b"\xfb\x2a\xf5\x68\xc0(.+)\x6e\x2f\x0f\xeb\x2d")
+
+        try:
+            firmware_file = open(path, "rb")
+        except Exception as err:
+            print("Error: {0}".format(err))
+            return
+
+        firmware_binary = firmware_file.read()
+        firmware_file.close()
+
+        # read the contents of firmware into buffer
+        regex_name_result = regex_name.search(firmware_binary)
+        regex_version_result = regex_version.search(firmware_binary)
+
+        if not regex_SmartIoT.search(firmware_binary) or not regex_name_result or not regex_version_result:
+            print(path + "file is not a valid SmartIot firmware")
+            return 
+
+        regex_brand_result = regex_brand.search(firmware_binary)
+        name = regex_name_result.group(1).decode()
+        version = regex_version_result.group(1).decode()
+        brand = regex_brand_result.group(1).decode() if regex_brand_result else "SmartIot"
+
+        fwbytear = bytearray()
+        fwbytear.extend(firmware_binary)
+
+        firmware["name"] = name
+        firmware["version"] = version
+        firmware["brand"] = brand
+        firmware["int_version"] = self.version_toint(version)
+        firmware["path"] = path
+        firmware["md5"] = md5(fwbytear).hexdigest()
+
+        if (firmware["name"] not in self.firmwares) or \
+        ((self.firmwares[firmware["name"]]["int_version"]<firmware["int_version"])):
+                self.firmwares[firmware["name"]] = firmware
+                self.firmwares[firmware["name"]]["published"]=False
+                print(path + " file included in firmware list")
+                return True
+        else:
+            print(path + " file outdated, not included  in firmware list")
+            return False
+
+    def _read_firmware_folder(self):
+        for filename in os.listdir(self._directory):
+            if (filename.endswith(".bin")):
+                self._update_firmware_info(os.path.join(self._directory, filename))
+
+
+
+        
+
